a47300eb1cdc9db164ab431cf661091168a39436
touhid2003 <touhidislam2003@gmail.com>
2026-09-05 13:13:49 +0600

    sep-5

---
+++ sayHello.js
@@ -0,0 +1,13 @@
+ // function creating
+        function sayHello(){
+            alert ("Hello world");
+        }
+
+        // function return
+
+        function touhid(){
+            return "I am a Md. Touhidur Islam";
+        }
+        let massage = touhid();
+
+        
